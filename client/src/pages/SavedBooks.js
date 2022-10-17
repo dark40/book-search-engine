ab480499c06a14d1b873679af67e9822b96bce53
Freddie Yang <dark_yty@hotmail.com>
2022-10-17 19:06:08 +1100

    added to heroku

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import { useMutation, useQuery } from "@apollo/react-hooks";
+import React from "react";
+import {
+  Jumbotron,
+  Container,
+  CardColumns,
+  Card,
+  Button,
+} from "react-bootstrap";
 import { GET_ME } from "../utils/queries";
 import { REMOVE_BOOK } from "../utils/mutations";
-import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-
-// import { getMe, deleteBook } from '../utils/API';
-import Auth from '../utils/auth';
-import { removeBookId } from '../utils/localStorage';
+import Auth from "../utils/auth";
+import { removeBookId } from "../utils/localStorage";
+import { useQuery, useMutation } from "@apollo/react-hooks";
 
 const SavedBooks = () => {
-
-  // const [userData, setUserData] = useState({});
   const { loading, data } = useQuery(GET_ME);
-  const userData = data?.me || [];
-  // const loggedIn = Auth.loggedIn();
-  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
+  let userData = data?.me || {};
+  console.log(userData);
+  const [removeBook] = useMutation(REMOVE_BOOK);
 
-  // create function that accepts the book's mongo _id value as param and deletes the book from the database
+  // function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
@@ -25,49 +27,61 @@ const SavedBooks = () => {
     }
 
     try {
-      await removeBook({
-        variables: { bookId },
+      const { user } = await removeBook({
+        variables: {
+          bookId: bookId,
+        },
       });
-      // upon success, remove book's id from localStorage
+
+      userData = user;
       removeBookId(bookId);
-      window.location.reload();
     } catch (err) {
       console.error(err);
     }
   };
 
+  // if data isn't here yet, say so
   if (loading) {
-    return <div>Loading...</div>;
+    return <h2>LOADING...</h2>;
   }
-  
+
   return (
     <>
-      <Jumbotron fluid className='text-light bg-dark'>
+      <Jumbotron fluid className="text-light bg-dark">
         <Container>
           <h1>Viewing saved books!</h1>
         </Container>
       </Jumbotron>
       <Container>
         <h2>
-          {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
-            : 'You have no saved books!'}
+          {userData.savedBooks?.length
+            ? `Viewing ${userData.savedBooks.length} saved ${
+                userData.savedBooks.length === 1 ? "book" : "books"
+              }:`
+            : "You have no saved books!"}
         </h2>
         <CardColumns>
-          {userData.savedBooks.map((book) => {
+          {userData.savedBooks?.map((book) => {
             return (
-              <Card key={book.bookId} border='dark'>
-                {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
+              <Card key={book.bookId} border="dark">
+                {book.image ? (
+                  <Card.Img
+                    src={book.image}
+                    alt={`The cover for ${book.title}`}
+                    variant="top"
+                  />
+                ) : null}
                 <Card.Body>
                   <Card.Title>{book.title}</Card.Title>
-                  <p className='small'>Authors: {book.authors}</p>
-                  {book.link ? <p className='small'><a href={book.link} target="_blank" rel="noopener noreferrer">Link: {book.link}</a></p> : <p className='small'>No link to Google Books found.</p>}
+                  <p className="small">Authors: {book.authors}</p>
                   <Card.Text>{book.description}</Card.Text>
-                  <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                  <Button
+                    className="btn-block btn-danger"
+                    onClick={() => handleDeleteBook(book.bookId)}
+                  >
                     Delete this Book!
                   </Button>
                 </Card.Body>
-                {error && <div> Something went wrong...</div>}
               </Card>
             );
           })}
